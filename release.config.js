7b16b96d36d029d43533b7fda4972a553883961a
mustapha garba <musgab3@gmail.com>
2023-10-01 16:08:37 -0400

    fix: workflow

--- release.config.js
+++ release.config.js
@@ -1,11 +1,11 @@
 module.exports = {
     branches: "main",
-    repositoryurl: "https://github.com/Mus865/Gitdemo.git"
+    repositoryUrl: "https://github.com/Mus865/Gitdemo.git",
     plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    "@semantic-release/npm",
-    "@semantic-release/github" 
+      "@semantic-release/commit-analyzer",
+      "@semantic-release/release-notes-generator",
+      "@semantic-release/npm",
+      "@semantic-release/github" 
     ]
-    
-    }
+  };
+  
